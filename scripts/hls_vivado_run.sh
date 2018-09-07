ec79b5cd4c9ae46ba510d6ceb1831ab2aaf5add7
Bernhard Arnold <arnobaer@users.noreply.github.com>
2018-09-07 15:39:24 +0200

    moved generating IP core to startSynth.py

--- scripts/hls_vivado_run.sh
+++ scripts/hls_vivado_run.sh
@@ -3,6 +3,8 @@ if [ "$1" == "-h" ]; then
   echo "Usage: `basename $0` l1menu_path l1menu_name vivado_version build_dir build_nr"
   exit 0
 fi
+username=herbberg
+tag=master
 l1menu_path="$1"
 echo $l1menu_path
 l1menu_name="$2"
@@ -15,12 +17,12 @@ build="$5"
 echo $build
 echo ""
 echo "------------------"
-git clone https://github.com/herbberg/hls4gtl ~/$build_dir/hls4gtl
+git clone https://github.com/$username/hls4gtl ~/$build_dir/hls4gtl
 cd ~/$build_dir/hls4gtl
+git checkout $tag
 python utils/distribute.py ~/$l1menu_path/$l1menu_name/xml/$l1menu_name.xml
 cd
-git clone https://github.com/herbberg/mp7ugt_hls ~/$build_dir/mp7ugt_hls
+git clone https://github.com/$username/mp7ugt_hls ~/$build_dir/mp7ugt_hls
 python ~/$build_dir/mp7ugt_hls/scripts/makeProject.py -t mp7fw_v2_4_1 -u hbergaue -b 0x$build -m ~/$l1menu_path/$l1menu_name --hls ~/$build_dir/hls4gtl/hls_impl/solution1/impl/ip -p ~/$build_dir/work
 cd ~/$build_dir/hls4gtl
-vivado_hls -f run_hls.tcl
-python ~/$build_dir/mp7ugt_hls/scripts/startSynth.py $vivado_version ~/$build_dir/work/mp7_ugt/0x$build/mp7fw_v2_4_1/build/build_0x$build.cfg --vivado_base_dir /opt/Xilinx/Vivado
+python ~/$build_dir/mp7ugt_hls/scripts/startSynth.py $vivado_version ~/$build_dir/work/mp7_ugt/0x$build/mp7fw_v2_4_1/build/build_0x$build.cfg
